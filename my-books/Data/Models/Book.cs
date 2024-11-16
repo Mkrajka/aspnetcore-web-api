9cbf195dc077d4d6b090704322d89a7734434c0c
Mario Krajka <mkrajka21@beder.edu.al>
2024-11-16 22:03:13 +0100

    This is first commit

---
+++ my-books/Data/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿namespace my_books.Data.Models
+{
+    public class Book
+    {
+
+        public int Id { get; set; } 
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsRead { get; set; }
+
+        public int? Rate { get; set; }
+
+        public string CoverUrl { get; set; }
+
+        public string Genre {  get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime? DateRead { get; set; } //nese i shotjm pikpytje nfund e bejm opsionale
+
+       
+
+
+    }
+}
---
+++ my-books/Startup.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using my_books.Data;
+
+namespace my_books
+{
+    public class Startup
+{
+
+        public string ConnectionString { get; set; }
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+            ConnectionString = Configuration.GetConnectionString("DefaultConnectionString");
+    }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+            // Configure DbContext with SQL Server
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+        // Add services for controllers and views
+        services.AddControllersWithViews();
+
+        // Add services for Razor Pages (if needed)
+        services.AddRazorPages();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            // Configure endpoints for MVC
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            // Map Razor Pages
+            endpoints.MapRazorPages();
+        });
+    }
+}
+}
